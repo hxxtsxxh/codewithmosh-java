9f5857c354ffdf1a219470e47d36a05b5bf62590
Heet Shah <heet1356@gmail.com>
2021-05-18 16:12:49 -0400

    Updated

--- src/com/heetshah/Main.java
+++ src/com/heetshah/Main.java
@@ -1,8 +1,36 @@
 package com.heetshah;
 
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        /*
+        Simple Interest Calculator
+            - Principal: initial amount
+            - Rate: rate of interest per year in decimal
+            - Time/Period: how many years
+            Formula: P x A x R
+         */
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.print("Rate: ");
+        double ir = scanner.nextDouble();
+
+        System.out.print("Period (Years): ");
+        byte time = scanner.nextByte();
+
+
+
+        var formula = principal * (ir/100) * time;
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        System.out.println("Simple Interest: " + currency.format(formula));
+
     }
 }
